ea8a6828c270b064d267854e9853f3184d04961a
Mersy-Dev <anuoluwafalaju@gmail.com>
2023-08-06 22:06:28 +0100

    Updated to Services Section

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,8 +1,55 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUser} from 'react-icons/fi'
+import { VscFolderLibrary} from 'react-icons/vsc'
+
+
+
+
+
+
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUser className='about__icon' />
+              <h5>clients</h5>
+              <small>100+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto ad veritatis ab similique voluptas minima dolorum, nobis vero, doloremque suscipit nesciunt asperiores hic quasi ut neque repudiandae? Aperiam, soluta molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          <a href="#contact" className='btn btn-primary'> Lets Talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './header.css'
+import CTA from './CTA.js'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>MERCY DEV</h1>
+                <h5 className='text-light'>Fullstack Developer</h5>
+                <CTA/>
+                <HeaderSocial/> 
+
+                <div className="me"> 
+                    <img src={ME} alt="me" />
+                </div>
+                <a href="#contact" className='scroll__down'>Scroll Dowm</a>
+            </div>
+        </header>
+
+    )
 }
 
 export default Header
